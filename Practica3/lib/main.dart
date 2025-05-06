cfade550c56b623deb23f2aa5a70b2686eb0d20a
Adam Navarro Megías <145340543+Adman23@users.noreply.github.com>
2025-05-07 01:04:21 +0200

    Main está hecho, falta simplemente gestión de errores (No comete errores pero estaría bien que saliese un pop up si hay uno)

--- Practica3/lib/main.dart
+++ Practica3/lib/main.dart
@@ -34,6 +34,45 @@ class BankWidget extends StatefulWidget {
 class _BankWidget extends State<BankWidget> {
 
   BankService bank = BankService();
+  final ScrollController _scrollController = ScrollController();
+  final _valueControllerDeposit = TextEditingController();
+  final _valueControllerWithdrawal = TextEditingController();
+  final _valueControllerTransfer = TextEditingController();
+
+  String _selectedAccountDeposit = "";
+  String _selectedAccountWithdrawal = "";
+  String _selectedAccountTransferFrom = "";
+  String _selectedAccountTransferTo = "";
+  double? _amountDeposit = 0;
+  double? _amountWithdrawal = 0;
+  double? _amountTransfer = 0;
+
+  @override
+  void dispose(){
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  Future<String?> _showSelectionDialog(BuildContext context, Iterable<String> items) {
+    return showModalBottomSheet<String>(
+      context: context,
+      builder: (context) {
+        return ListView.builder(
+          itemCount: items.length,
+          itemBuilder: (context, index) {
+            final item = items.elementAt(index);
+            return ListTile(
+              title: Text("Número de cuenta: $item"),
+              onTap: () {
+                Navigator.pop(context, item);
+              },
+            );
+          },
+        );
+      },
+    );
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -68,17 +107,42 @@ class _BankWidget extends State<BankWidget> {
                                     right: BorderSide(color: Colors.grey),),
                     ),
                     child: Center(
-                      child: ListView.builder(
-                        scrollDirection: Axis.horizontal,
-                        shrinkWrap: true,
-                        itemCount: bank.accounts.keys.length,
-                        itemBuilder: (context, idx){
-                          return Center(
-                                    child: Padding(padding:EdgeInsets.all(10.0),
-                                           child:Text(bank.accounts.keys.elementAt(idx))),
-                                  );
-                        },
-                      ),
+                      child: RawScrollbar(
+                        controller: _scrollController,
+                        radius: const Radius.circular(10),
+                        thickness: 10,
+                        thumbColor: Colors.indigo,
+                        child: ListView.builder(
+                          controller: _scrollController,
+                          scrollDirection: Axis.horizontal,
+                          shrinkWrap: true,
+                          itemCount: bank.accounts.keys.length,
+                          itemBuilder: (context, idx){
+                            return Center(
+                              child: Padding(
+                                padding:EdgeInsets.all(10.0),
+                                child: Container(
+                                  alignment: Alignment.center,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(10),
+                                    border: Border(top: BorderSide(color: Colors.grey),
+                                      bottom: BorderSide(color: Colors.grey),
+                                      left: BorderSide(color: Colors.grey),
+                                      right: BorderSide(color: Colors.grey),),
+                                  ),
+                                  child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    crossAxisAlignment: CrossAxisAlignment.center,
+                                    children: [
+                                      Padding(padding: EdgeInsets.only(top: 10, left:10, right: 10), child: Text("Account: ${bank.accounts.keys.elementAt(idx)}")),
+                                      Padding(padding: EdgeInsets.all(10.0), child: Text("Balance: ${bank.getBalance(bank.accounts.keys.elementAt(idx))}")),                                   ],
+                                  ),
+                                ),
+                              ),
+                            );
+                          },
+                        ),
+                      )
                     ),
                   )
                 ),
@@ -103,19 +167,68 @@ class _BankWidget extends State<BankWidget> {
             ),
           ),
 
+
+
           // BLOQUE INGRESAR DINERO
           Padding(
             padding: const EdgeInsets.all(20.0),
             child: Row(
               children: [
-                // Campos de valor
-                //
+                // SELECCIÓN DE LA CUENTA A DEPOSITAR
+                ElevatedButton(
+                  onPressed: () async {
+                    final selected = await _showSelectionDialog(context, bank.accounts.keys);
+                    setState(() {
+                      _selectedAccountDeposit = selected!;
+                    });
+                    if (selected != null) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(content: Text('Seleccionaste: $selected')),
+                      );
+                    }
+                  },
+                  child: const Text('Seleccionar cuenta'),
+                ),
 
+                // MOSTRAR LA SELECCIÓN
+                Padding(
+                  padding: EdgeInsets.only(left: 20, right: 20),
+                  child: Center(
+                    child: Text("Seleccionada: $_selectedAccountDeposit"),
+                  ),
+                ),
 
+                // INTRODUCIR CANTIDAD A INGRESAR
+                Expanded(
+                  flex:1,
+                  child: Padding(
+                    padding: EdgeInsets.only(left:20, right: 20),
+                    child: TextField(
+                      controller: _valueControllerDeposit,
+                      onChanged: (value) {
+                        setState(() {
+                          _amountDeposit = double.tryParse(_valueControllerDeposit.text);
+                        });
+                      },
+                      decoration: InputDecoration(
+                        labelText: 'Introduce cantidad',
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                  ),
+                ),
+
+                // INGRESAR EL DINERO
                 ElevatedButton(
                   onPressed: () {
                     setState(() {
-                      // Codigo de ingresar, tiene que coger valores de los otros campos
+                      if (_selectedAccountDeposit != "") {
+                        if (_amountDeposit != null && _amountDeposit != 0) {
+                          bank.deposit(_selectedAccountDeposit, _amountDeposit!);
+                          _valueControllerDeposit.clear();
+                          _selectedAccountDeposit = "";
+                        }
+                      }
                     });
                   },
                   child: Text('Ingresar'),
@@ -130,22 +243,65 @@ class _BankWidget extends State<BankWidget> {
             padding: const EdgeInsets.all(20.0),
             child: Row(
               children: [
-                Text("COSA1"),
-                Text("COSA2"),
-                Text("COSA3"),
-                Text("COSA4"),
-                Text("COSA5"),
-                Container(
-                  height: 100,
-                  alignment: Alignment.center,
-                  decoration: BoxDecoration(
-                    border: Border(top: BorderSide(color: Colors.grey),
-                      bottom: BorderSide(color: Colors.grey),
-                      left: BorderSide(color: Colors.grey),
-                      right: BorderSide(color: Colors.grey),),
+                // SELECCIÓN DE LA CUENTA A DEPOSITAR
+                ElevatedButton(
+                  onPressed: () async {
+                    final selected = await _showSelectionDialog(context, bank.accounts.keys);
+                    setState(() {
+                      _selectedAccountWithdrawal = selected!;
+                    });
+                    if (selected != null) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(content: Text('Seleccionaste: $selected')),
+                      );
+                    }
+                  },
+                  child: const Text('Seleccionar cuenta'),
+                ),
+
+                // MOSTRAR LA SELECCIÓN
+                Padding(
+                  padding: EdgeInsets.only(left: 20, right: 20),
+                  child: Center(
+                    child: Text("Seleccionada: $_selectedAccountWithdrawal"),
                   ),
-                  child: Text("COSA9"),
-                )
+                ),
+
+                // INTRODUCIR CANTIDAD A RETIRAR
+                Expanded(
+                  flex:1,
+                  child: Padding(
+                    padding: EdgeInsets.only(left:20, right: 20),
+                    child: TextField(
+                      controller: _valueControllerWithdrawal,
+                      onChanged: (value) {
+                        setState(() {
+                          _amountWithdrawal = double.tryParse(_valueControllerWithdrawal.text);
+                        });
+                      },
+                      decoration: InputDecoration(
+                        labelText: 'Introduce cantidad',
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                  ),
+                ),
+
+                // RETIRAR EL DINERO
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      if (_selectedAccountWithdrawal != "") {
+                        if (_amountWithdrawal != null && _amountWithdrawal != 0) {
+                          bank.withdraw(_selectedAccountWithdrawal, _amountWithdrawal!);
+                          _valueControllerWithdrawal.clear();
+                          _selectedAccountWithdrawal = "";
+                        }
+                      }
+                    });
+                  },
+                  child: Text('Retirar'),
+                ),
               ],
             ),
           ),
@@ -156,22 +312,90 @@ class _BankWidget extends State<BankWidget> {
             padding: const EdgeInsets.all(20.0),
             child: Row(
               children: [
-                Text("COSA1"),
-                Text("COSA2"),
-                Text("COSA3"),
-                Text("COSA4"),
-                Text("COSA5"),
-                Container(
-                  height: 100,
-                  alignment: Alignment.center,
-                  decoration: BoxDecoration(
-                    border: Border(top: BorderSide(color: Colors.grey),
-                      bottom: BorderSide(color: Colors.grey),
-                      left: BorderSide(color: Colors.grey),
-                      right: BorderSide(color: Colors.grey),),
+                // SELECCIÓN DE LA CUENTA ORIGEN
+                ElevatedButton(
+                  onPressed: () async {
+                    final selected = await _showSelectionDialog(context, bank.accounts.keys);
+                    setState(() {
+                      _selectedAccountTransferFrom = selected!;
+                    });
+                    if (selected != null) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(content: Text('Seleccionaste: $selected')),
+                      );
+                    }
+                  },
+                  child: const Text('Cuenta Origen'),
+                ),
+                // MOSTRAR LA SELECCIÓN
+                Padding(
+                  padding: EdgeInsets.only(left: 20, right: 20),
+                  child: Center(
+                    child: Text(_selectedAccountTransferFrom),
                   ),
-                  child: Text("COSA9"),
-                )
+                ),
+
+                // SELECCIÓN DE LA CUENTA DESTINO
+                ElevatedButton(
+                  onPressed: () async {
+                    final selected = await _showSelectionDialog(context, bank.accounts.keys);
+                    setState(() {
+                      _selectedAccountTransferTo = selected!;
+                    });
+                    if (selected != null) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(content: Text('Seleccionaste: $selected')),
+                      );
+                    }
+                  },
+                  child: const Text('Cuenta Origen'),
+                ),
+
+                // MOSTRAR LA SELECCIÓN
+                Padding(
+                  padding: EdgeInsets.only(left: 20, right: 20),
+                  child: Center(
+                    child: Text(_selectedAccountTransferTo),
+                  ),
+                ),
+
+
+                // INTRODUCIR CANTIDAD A TRANSFERIR
+                Expanded(
+                  flex:1,
+                  child: Padding(
+                    padding: EdgeInsets.only(left:20, right: 20),
+                    child: TextField(
+                      controller: _valueControllerTransfer,
+                      onChanged: (value) {
+                        setState(() {
+                          _amountTransfer = double.tryParse(_valueControllerTransfer.text);
+                        });
+                      },
+                      decoration: InputDecoration(
+                        labelText: 'Introduce cantidad',
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                  ),
+                ),
+
+                // TRANSFERIR EL DINERO
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      if (_valueControllerTransfer != "") {
+                        if (_amountTransfer != null && _amountTransfer != 0) {
+                          bank.transfer(_selectedAccountTransferFrom, _selectedAccountTransferTo, _amountTransfer!);
+                          _valueControllerTransfer.clear();
+                          _selectedAccountTransferTo = "";
+                          _selectedAccountTransferFrom = "";
+                        }
+                      }
+                    });
+                  },
+                  child: Text('Transferir'),
+                ),
               ],
             ),
           )
@@ -180,4 +404,6 @@ class _BankWidget extends State<BankWidget> {
       ),
     );
   }
+
+
 }
