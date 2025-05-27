ace2202474fc2fa90066c789688c5d4d5e694ea2
Adam Navarro Megías <145340543+Adman23@users.noreply.github.com>
2025-05-27 10:47:58 +0200

    a

--- triaccount/triaccount_api/app/models/group.rb
+++ triaccount/triaccount_api/app/models/group.rb
@@ -1,5 +1,5 @@
 class Group < ApplicationRecord
     has_many :memberships
     has_many :users, through: :memberships
-    has_many :expenses
+    has_many :expenses, -> { order(date: :desc) }, dependent: :destroy
 end
